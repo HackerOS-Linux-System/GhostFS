57c7658cc096d689384f7a14481bde72854ae172
michal92299 <chudybyczek2137@gmail.com>
2026-03-25 16:40:36 +0100

    Update xattr.rs

--- source-code/src/xattr.rs
+++ source-code/src/xattr.rs
@@ -1,33 +1,37 @@
 use sled::Db;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 use std::os::unix::ffi::OsStrExt;
+use crate::error::HfsError;
 
 pub struct XAttr {
     db: Db,
 }
 
 impl XAttr {
-    pub fn new(db: &Db) -> Result<Self, ()> {
+    pub fn new(db: &Db) -> Result<Self, HfsError> {
         Ok(Self { db: db.clone() })
     }
 
-    pub fn get(&self, ino: u64, name: &OsStr) -> Result<Option<Vec<u8>>, ()> {
+    /// Get the value of an extended attribute.
+    pub fn get(&self, ino: u64, name: &OsStr) -> Result<Option<Vec<u8>>, HfsError> {
         let key = format!("xattr:{}:{}", ino, String::from_utf8_lossy(name.as_bytes()));
-        self.db.get(key.as_bytes()).map(|opt| opt.map(|v| v.to_vec())).map_err(|_| ())
+        Ok(self.db.get(key.as_bytes())?.map(|v| v.to_vec()))
     }
 
-    pub fn set(&self, ino: u64, name: &OsStr, value: &[u8]) -> Result<(), ()> {
+    /// Set an extended attribute (overwrites if exists).
+    pub fn set(&self, ino: u64, name: &OsStr, value: &[u8]) -> Result<(), HfsError> {
         let key = format!("xattr:{}:{}", ino, String::from_utf8_lossy(name.as_bytes()));
-        self.db.insert(key.as_bytes(), value).map_err(|_| ())?;
+        self.db.insert(key.as_bytes(), value)?;
         Ok(())
     }
 
-    pub fn list(&self, ino: u64) -> Result<Vec<OsString>, ()> {
+    /// List all extended attribute names for an inode.
+    pub fn list(&self, ino: u64) -> Result<Vec<OsString>, HfsError> {
         let prefix = format!("xattr:{}:", ino);
         let mut names = Vec::new();
         for item in self.db.scan_prefix(prefix.as_bytes()) {
-            let (k, _) = item.map_err(|_| ())?;
-            let k_str = String::from_utf8(k.to_vec()).map_err(|_| ())?;
+            let (k, _) = item?;
+            let k_str = String::from_utf8(k.to_vec())?;
             if let Some(suffix) = k_str.strip_prefix(&prefix) {
                 names.push(OsString::from(suffix));
             }
@@ -35,9 +39,10 @@ impl XAttr {
         Ok(names)
     }
 
-    pub fn remove(&self, ino: u64, name: &OsStr) -> Result<(), ()> {
+    /// Remove an extended attribute.
+    pub fn remove(&self, ino: u64, name: &OsStr) -> Result<(), HfsError> {
         let key = format!("xattr:{}:{}", ino, String::from_utf8_lossy(name.as_bytes()));
-        self.db.remove(key.as_bytes()).map_err(|_| ())?;
+        self.db.remove(key.as_bytes())?;
         Ok(())
     }
 }
